7422ce52ea5f4676cbc28227e32de05af3160ba8
abdomostafa <https://github.com/Abdo-Mostafa2458/flutter_quran_app.git>
2024-08-27 10:43:52 +0300

    +create project

---
+++ bin/max_min_number_with_ternary_operator.dart
@@ -0,0 +1,5 @@
+import 'package:max_min_number_with_ternary_operator/max_min_number_with_ternary_operator.dart' as max_min_number_with_ternary_operator;
+
+void main(List<String> arguments) {
+  print('Hello world: ${max_min_number_with_ternary_operator.calculate()}!');
+}
