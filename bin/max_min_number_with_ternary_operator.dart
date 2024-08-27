0ae344c5aea1a931e7087c9e67a3596cc451d8a0
abdomostafa <https://github.com/Abdo-Mostafa2458/flutter_quran_app.git>
2024-08-27 10:48:28 +0300

    + update + create a max number condition with ternay operator + create a min number condition with ternay operator

--- bin/max_min_number_with_ternary_operator.dart
+++ bin/max_min_number_with_ternary_operator.dart
@@ -1,5 +1,49 @@
-import 'package:max_min_number_with_ternary_operator/max_min_number_with_ternary_operator.dart' as max_min_number_with_ternary_operator;
+import 'dart:io';
 
-void main(List<String> arguments) {
-  print('Hello world: ${max_min_number_with_ternary_operator.calculate()}!');
+void main() {
+  int number_1 = int.parse(stdin.readLineSync()); //6
+  int number_2 = int.parse(stdin.readLineSync()); //4
+  int number_3 = int.parse(stdin.readLineSync()); //2
+  int max, min = 0;
+  max = (number_1 > number_2)
+      ? (number_1 > number_3 ? number_1 : number_3)
+      : (number_2 > number_1)
+      ? (number_2 > number_3 ? number_2 : number_3)
+      : number_3;
+  // if (number_1 > number_2) {
+  //   if (number_1 > number_3) {
+  //     max = number_1;
+  //     if (number_2 < number_3) {
+  //       min = number_2;
+  //     } else
+  //       min = number_3;
+  //   }
+  // }
+  // if (number_2 > number_1) {
+  //   if (number_2 > number_1) {
+  //     max = number_2;
+  //     if (number_1 < number_3) {
+  //       min = number_1;
+  //     } else
+  //       min = number_3;
+  //   }
+  // }
+  //
+  // if (number_3 > number_2) {
+  //   if (number_3 > number_1) {
+  //     max = number_3;
+  //     if (number_2 < number_1) {
+  //       min = number_2;
+  //     } else
+  //       min = number_1;
+  //   }
+  // }
+
+  min = (number_1 < number_2)
+      ? (number_1 < number_3 ? number_1 : number_3)
+      : (number_2 < number_3)
+      ? (number_2 < number_1 ? number_2 : number_1)
+      : number_3;
+
+  print("max = $max,   min = $min");
 }
